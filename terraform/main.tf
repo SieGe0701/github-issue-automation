8b4b1cc7f52c7f3087d4528048e8f8faf154aff7
R Srinivas Prabhu <116585928+SieGe0701@users.noreply.github.com>
2025-02-19 11:00:29 +0530

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,24 @@
+locals {
+  config_data = yamldecode(file("../specifications/config.yml"))
+}
 
+
+provider "github" {
+  token = var.github_token
+}
+
+resource "github_issue" "automated_issue" {
+  repository = "issue_poc_check"   # Replace with your repository name
+  title      = var.title
+
+  body = <<EOT
+### Input A
+${local.config_data["A"]["input_a"]}
+
+### Input B
+${local.config_data["A"]["input_b"]}
+
+### Input C
+${local.config_data["A"]["input_c"]}
+EOT
+}
